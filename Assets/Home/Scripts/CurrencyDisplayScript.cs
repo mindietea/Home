b1ee331c6eac2052cd8576cb8d6967a749bfeb07
mindychi <mindychi@gmail.com>
2019-01-26 01:15:21 +0800

    Menu has a scroll, player currency script will update to match the value stored in PlayerStats

---
+++ Assets/Home/Scripts/CurrencyDisplayScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrencyDisplayScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateLogs();
+        UpdateRocks();
+        UpdateStars();
+    }
+
+    void UpdateLogs()
+    {
+        GameObject logsCost = transform.Find("Logs/LogsCost").gameObject;
+        logsCost.GetComponent<Text>().text = GameObject.Find("PlayerStats").GetComponent<PlayerStats>().logs.ToString();
+    }
+
+    void UpdateRocks()
+    {
+        GameObject rocksCost = transform.Find("Rocks/RocksCost").gameObject;
+        rocksCost.GetComponent<Text>().text = GameObject.Find("PlayerStats").GetComponent<PlayerStats>().rocks.ToString();
+    }
+
+    void UpdateStars()
+    {
+        GameObject starsCost = transform.Find("Stars/StarsCost").gameObject;
+        starsCost.GetComponent<Text>().text = GameObject.Find("PlayerStats").GetComponent<PlayerStats>().stars.ToString();
+    }
+}
