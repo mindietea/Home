f00e3eadcfc3f513e0b9cdfc7de7c2cdeaca9f22
calcarlson <calcarlson@gmail.com>
2019-01-26 14:04:17 +0800

    added movement

---
+++ Assets/Home/Scripts/Movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour // Normal Movements Variables
+{
+    public float speed;
+    public Transform Player;
+    private Rigidbody2D rb2d;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W)) {
+            transform.Translate (Vector2.up * speed);       
+            //transform.position += character1.up * speed;
+
+            //Rigidbody2D.Addforce(character1.up);
+            //Rigidbody2D.velocity = character1.up;
+        }
+       
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Translate (Vector3.left * speed);       
+            //transform.position +=character1.left * speed;
+
+            //Rigidbody2D.Addforce(character1.left);
+            //Rigidbody2D.velocity = character1.left;
+        }
+
+        if (Input.GetKey(KeyCode.S)) {
+            transform.Translate (Vector2.down * speed);       
+            //transform.position +=character1.down * speed;
+
+            //Rigidbody2D.Addforce(character1.down);
+            //Rigidbody2D.velocity = character1.down;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            transform.Translate (Vector3.right * speed);       
+            //transform.position +=character1.right * speed;
+
+            //Rigidbody2D.Addforce(character1.right);
+            //Rigidbody2D.velocity = character1.down;
+        }
+        
+    }
+}
