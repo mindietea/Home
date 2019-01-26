6eecba1bb639be1fa498f5466f425d77e84828af
mindychi <mindychi@gmail.com>
2019-01-26 22:00:05 +0800

    Working on projectile

---
+++ Assets/Battle/Scripts/TreeProjectileScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* 
+ * A projectile that runs for its maxDistance then stops. If droppable, will become a wood pickup once it stops.
+ * Damages the Player.
+ */
+public class TreeProjectileScript : MonoBehaviour
+{
+    public float maxDistance = 10.0f;
+    public Vector2 direction = new Vector2(1.0f, 0.0f);
+    public float speed = 2.0f;
+    public bool droppable = true;
+    public bool dropped = false;
+
+    private Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        direction.Normalize();
+    }
+
+    void FixedUpdate()
+    {
+
+        if(!dropped && GetDistanceTraveled() > maxDistance)
+        {
+            // Drop or destroy
+            if(droppable)
+            {
+                dropped = true;
+            } else
+            {
+                Destroy(gameObject);
+            }
+
+        } else if(!dropped)
+        {
+            Vector3 direction3d = new Vector3((direction * speed).x, (direction * speed).y, 0);
+            transform.position += direction3d;
+        }
+    }
+
+    float GetDistanceTraveled()
+    {
+        return Vector2.Distance(transform.position, startPos);
+    }
+}
---
+++ Assets/Battle/Scripts/TreeScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeScript : MonoBehaviour
+{
+
+    public float projectileCooldown = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
