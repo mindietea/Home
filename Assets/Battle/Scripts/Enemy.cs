3c7e6db081deec7a79eed66150949c0cf1996974
calcarlson <calcarlson@gmail.com>
2019-01-26 21:00:16 +0800

    scipts

---
+++ Assets/Battle/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public float speed;
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("isRunning", true);
+
+    }
+    
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        Debug.Log("damage Taken");
+    }
+}
