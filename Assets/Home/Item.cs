8ab2a4b5d9e00e9400918b0fd936b143d5f87d15
mindychi <mindychi@gmail.com>
2019-01-26 11:13:19 +0800

    Can now purchase things and they appear. Unbuyable if can't afford or already purchased

---
+++ Assets/Home/Item.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    public string name;
+    public string flavorText;
+    public int logs;
+    public int rocks;
+    public int stars;
+
+    public Item CreateItem(string name, string flavorText, int logs, int rocks, int stars)
+    {
+        Item item = new Item();
+        item.name = name;
+        item.flavorText = flavorText;
+        item.logs = logs;
+        item.rocks = rocks;
+        item.stars = stars;
+
+        return item;
+    }
+}
---
+++ Assets/Home/Scripts/ItemScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemScript : MonoBehaviour
+{
+
+    private bool bought = false;
+
+    public GameObject furnitureDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(furnitureDisplay == null)
+        {
+            Debug.LogWarning("WARNING: Your ItemScript doesn't have a furnitureDisplay attached!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!bought && CanAfford())
+        {
+            gameObject.GetComponent<CanvasGroup>().alpha = 1.0f;
+        } else
+        {
+            gameObject.GetComponent<CanvasGroup>().alpha = 0.5f;
+        }
+    }
+
+    private bool CanAfford()
+    {
+        return GameObject.Find("PlayerStats").GetComponent<PlayerStats>().CanAfford(GetLogsCost(), GetRocksCost(), GetStarsCost());
+    }
+
+    private int GetLogsCost()
+    {
+        return int.Parse(transform.Find("Cost/Logs/LogsCost").gameObject.GetComponent<Text>().text);
+    }
+
+    private int GetRocksCost()
+    {
+        return int.Parse(transform.Find("Cost/Rocks/RocksCost").gameObject.GetComponent<Text>().text);
+    }
+
+    private int GetStarsCost()
+    {
+        return int.Parse(transform.Find("Cost/Stars/StarsCost").gameObject.GetComponent<Text>().text);
+    }
+
+    public void Buy()
+    {
+        if(!bought && CanAfford())
+        {
+            GameObject.Find("PlayerStats").GetComponent<PlayerStats>().logs -= GetLogsCost();
+            GameObject.Find("PlayerStats").GetComponent<PlayerStats>().rocks -= GetRocksCost();
+            GameObject.Find("PlayerStats").GetComponent<PlayerStats>().stars -= GetStarsCost();
+            bought = true;
+            furnitureDisplay.SetActive(true);
+        }
+    }
+}
