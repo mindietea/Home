160b627ba6130e9770e47521e14efdcf0bda98da
mindychi <mindychi@gmail.com>
2019-01-27 12:13:13 +0800

    Scripts for fading the craft view when mouse on it or not

---
+++ Assets/Home/Scripts/MouseFocusFadeScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.EventSystems;
+
+public class MouseFocusFadeScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //If your mouse hovers over the GameObject with the script attached, output this message
+        gameObject.GetComponent<CanvasGroup>().alpha = 1.0f;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //The mouse is no longer hovering over the GameObject so output this message each frame
+        gameObject.GetComponent<CanvasGroup>().alpha = 0.5f;
+    }
+}
