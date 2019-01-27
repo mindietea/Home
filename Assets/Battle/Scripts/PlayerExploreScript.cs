d6dfafc477ac1ecb5aad29509a58e11afd0e1e46
mindychi <mindychi@gmail.com>
2019-01-27 10:39:43 +0800

    Basic battle UI. Rock monster behavior done

--- Assets/Battle/Scripts/PlayerExploreScript.cs
+++ Assets/Battle/Scripts/PlayerExploreScript.cs
@@ -1,14 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerExploreScript : MonoBehaviour
 {
 
+    public int maxHealth = 20;
     public int health = 20;
     public int logs = 0;
     public int rocks = 0;
     public int stars = 0;
+
+    public void Start()
+    {
+        health = maxHealth;
+        GameObject.Find("MaxHealth").GetComponent<Text>().text = maxHealth.ToString();
+    }
+
+    public void Update()
+    {
+        GameObject.Find("CurrentHealth").GetComponent<Text>().text = health.ToString();
+
+    }
 
     public int Damage(int dmg)
     {
